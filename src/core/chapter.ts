93ac3662d1939734ff477452401020c843960e6e
Astral_Rider <tpx1008@163.com>
2025-04-01 11:28:56 +0800

    chore: better chapter format

--- src/core/chapter.ts
+++ src/core/chapter.ts
@@ -1,59 +1,56 @@
 import { RewriteText } from './text';
 
 export interface Talk {
+    type: 'talk';
     name: string;
     content: RewriteText[];
 }
 
 export type ChapterUnit = Talk;
 
+type LinkText = (some: TemplateStringsArray, ...values: RewriteText[]) => LinkText;
+type ChapterInfo = {
+    keys: string[];
+    characters: Map<string, string | null>;
+    memory: ChapterUnit[];
+};
+
 export function chapter() {
+    const keys: string[] = [];
     const characters = new Map<string, string | null>();
     const memory: ChapterUnit[] = [];
 
     class Chapter<T extends string = never> {
-        constructor(readonly keys: T[] = []) {}
+        constructor() {}
 
-        /**
-         * Adds a new character to the chapter with the specified name and title.
-         * @param name - The name identifier for the character
-         * @param title - The display title for the character, can be null
-         */
-        use<U extends string>(name: U, title: string | null): Chapter<T | U> {
+        use<U extends string>(name: U & (U extends T ? never : U), title: string | null): Chapter<T | U> {
+            keys.push(name);
             characters.set(name, title);
-            return new Chapter([...this.keys, name] as T[]);
+            return new Chapter();
         }
 
-        character(name: typeof this.getKeys extends () => (infer U)[] ? U : never) {
+        character(name: T): LinkText {
             function init(some: TemplateStringsArray, ...values: RewriteText[]) {
                 const content: RewriteText[] = [];
-                const talk: Talk = { name, content };
+                const talk: Talk = { type: 'talk', name, content };
                 memory.push(talk);
 
-                function link(some: TemplateStringsArray, ...values: RewriteText[]) {
+                function link(some: TemplateStringsArray, ...values: RewriteText[]): LinkText {
                     const len = some.length;
-
                     content.push(some[0]);
                     for (let i = 1; i < len; i++) {
                         content.push(values[i - 1]);
                         content.push(some[i]);
                     }
-
                     return link;
                 }
-
                 return link(some, ...values);
             }
-
             return init;
         }
 
-        getKeys(): T[] {
-            return this.keys;
-        }
-
-        EndOfChapter() {
-            return { characters, memory };
+        EndOfChapter(): ChapterInfo {
+            return { keys, characters, memory };
         }
     }
 
